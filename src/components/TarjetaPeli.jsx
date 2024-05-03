03c97e72a0018c46aed1cca5c087ce04d7a3943e
Alberto <a.g.arauzo13@gmail.com>
2024-05-03 10:41:13 +0200

    cambio tonto

--- src/components/TarjetaPeli.jsx
+++ src/components/TarjetaPeli.jsx
@@ -1,9 +1,9 @@
 export default function TarjetaPeli({title, year, foto}) {
     return(
-            <li>
-                <img className="w-64 h-96" src={foto == "N/A"? `https://pbs.twimg.com/media/GH0-jZrW0AAVg8y?format=jpg&name=360x360` : foto} alt={title} />
-                <p className="text-xl text-white">{title}</p>
-                <p className="text-xl text-white">{year}</p>
-            </li>
+        <li>
+            <img className="w-64 h-96" src={foto == "N/A"? `https://pbs.twimg.com/media/GH0-jZrW0AAVg8y?format=jpg&name=360x360` : foto} alt={title} />
+            <p className="text-xl text-white">{title}</p>
+            <p className="text-xl text-white">{year}</p>
+        </li>
     )
 }
